2d7b324055584c6eed1b7f84f26184fa37aa2ee3
harsh1602 <46862849+harsh1602@users.noreply.github.com>
2022-03-18 23:14:34 +0530

    Setup app for heroku

--- server.js
+++ server.js
@@ -3,23 +3,29 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
-//const particles=require('particles.js'); 
+//const particles = require('particles.js');  
 
 const connectDB = require('./server/database/connection');
 
 const app = express();
 
 dotenv.config( { path : 'config.env'} )
-const PORT = process.env.PORT || 8080 
+const PORT = process.env.PORT || 8080  
 
 // log requests
 app.use(morgan('tiny'));
 
-// mongodb connection
+// mongodb connection  
 connectDB();
 
 // parse request to body-parser
-app.use(bodyparser.urlencoded({ extended : true}))
+app.use(bodyparser.urlencoded({ extended : true})) 
+
+// app.get('/', function (req, res) {
+//   res.render("views/index", {
+//     particlesJS: particles 
+//   });
+// }) 
 
 // set view engine
 app.set("view engine", "ejs")
@@ -37,4 +43,4 @@ app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { 
   console.log('Server is running on http://localhost:'+PORT)
-});
+}); 
